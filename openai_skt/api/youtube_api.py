cb07505912f9f494941c7ba807d8874df32081ae
0601p <0601p@naver.com>
2023-09-01 03:24:02 +0900

    add youtube_api

--- openai_skt/api/youtube_api.py
+++ openai_skt/api/youtube_api.py
@@ -1,4 +1,12 @@
 from api.base import BaseAPI
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.tools import argparser
+import configparser
+
+config = configparser.ConfigParser()
+config.read('../.secrets.ini')
+YOUTUBE_KEY = config['YOUTUBE']['YOUTUBE_API_KEY']
 
 class YoutubeAPI(BaseAPI):
     # 유튜브 API
@@ -6,17 +14,34 @@ class YoutubeAPI(BaseAPI):
         super().__init__()
         self.base_url = 'https://www.youtube.com/'
         self.name = 'youtube'
+        self.youtube_api = build("youtube", "v3", developerKey=YOUTUBE_KEY)
 
-    def search(self, query):
-        pass
+    def search(self, query:str, top_k:int = 5):
+        response = self.youtube_api.search().list(
+            q = query,
+            order = 'relevance',
+            part = 'snippet',
+            maxResults = top_k
+        ).execute()
+        return self.parse_result(response)
 
-    async def async_search(self, query):
-        # Implement your asynchronous search logic here
-        pass
+    async def async_search(self, query:str, top_k:int = 5):
+        return self.search(query, top_k)
 
     def parse_result(self, result):
-        pass
+        ret = []
+        for item in result['items']:
+            if item['id']['kind'] == 'youtube#video':
+                ret.append({
+                    '제목': item['snippet']['title'],
+                    '날짜': item['snippet']['publishTime'],
+                    '채널': item['snippet']['channelTitle'],
+                    '설명': item['snippet']['description'],
+                    '링크': 'https://www.youtube.com/watch?v=' + item['id']['videoId'],
+                    'data_type': 'youtube_video',
+                    'data_path': 'https://www.youtube.com/watch?v=' + item['id']['videoId'],
+                })
+        return ret
 
     async def async_parse_result(self, result):
-        # Implement your asynchronous parse logic here
-        pass
+        return self.parse_result(result)
